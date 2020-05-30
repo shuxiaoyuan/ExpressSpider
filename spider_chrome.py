8392d3b38b5266c060ea0b2cc75cf2a2d8db8f30
shuyuan <1362521868@qq.com>
2020-05-30 17:36:12 +0800

    无界面模式 & 设置分辨率

--- spider_chrome.py
+++ spider_chrome.py
@@ -7,9 +7,13 @@
 
 import os
 import time
+import logging
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
 from PIL import Image
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 class ChromeSpider(Chrome):
@@ -17,11 +21,11 @@ class ChromeSpider(Chrome):
     def __init__(self):
         options = Options()
         options.add_argument('--no-sandbox')                        # 解决 DevToolsActivePort 文件不存在的报错
-        # options.add_argument('window-size=1920x3000')               # 指定浏览器分辨率
+        options.add_argument('window-size=1920x3000')               # 指定浏览器分辨率
         options.add_argument('--disable-gpu')                       # 谷歌文档提到需要加上这个属性来规避bug
         options.add_argument('--hide-scrollbars')                   # 隐藏滚动条, 应对一些特殊页面
         # options.add_argument('blink-settings=imagesEnabled=false')  # 不加载图片, 提升速度
-        # options.add_argument('--headless')                          # 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
+        options.add_argument('--headless')                          # 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
         # options.add_argument("user-data-dir=" + USER_DIR)            # 使用当前 chrome 的用户目录数据（需要关闭正在运行的 chrome）
         Chrome.__init__(self, executable_path='./chromedriver.exe', options=options)
 
@@ -29,18 +33,12 @@ class ChromeSpider(Chrome):
         self.__scrollTop = 0
         self.__input_express_num = None
         self.__button_search_express = None
-        self.maximize_window()
+        # self.maximize_window()
 
     def crawl(self, express_num_list):
 
         # 访问网站
         self.get('https://www.kuaidi100.com/?from=openv')
-
-        # 关闭弹窗红包
-        popup = self.find_element_by_class_name('coupon-pop-close')
-        if popup is not None:
-            popup.click()
-            time.sleep(0.5)
 
         # 定位元素并滑动到该位置
         self.__input_express_num = self.find_element_by_id('postid')
@@ -73,6 +71,7 @@ class ChromeSpider(Chrome):
         crop_image = Image.open('tmp.png')
         crop_image = crop_image.crop((left, top, right, bottom))
         crop_image.save(self.__path_save_images + express_num + '.png')
+        logger.info(self.__path_save_images + express_num + '.png saved.')
         os.remove('tmp.png')
 
 
